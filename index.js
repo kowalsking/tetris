4f981c305b8b780796691847b305787f7c17a2d9
Kozlenko <d.kozlenko@mediawayss.com>
2019-09-23 11:48:09 +0300

    Class View

--- index.js
+++ index.js
@@ -1,5 +1,32 @@
 import Game from './src/game.js';
+import View from './src/view.js';
+
+const root = document.querySelector('#root');
 
 const game = new Game();
+const view = new View(root, 320, 640, 20, 10);
 
-console.log(game);
+window.game = game;
+window.view = view;
+
+document.addEventListener('keydown', event => {
+    switch (event.keyCode) {
+        case 37: // LEFT ARROW
+            game.movePieceLeft();
+            view.render(game.getState());
+            break;
+        case 38: //UP
+            game.rotatePiece();
+            view.render(game.getState());
+            break;
+        case 39: //RIGHT
+            game.movePieceRight();
+            view.render(game.getState());
+            break;
+        case 40: // DOWN
+            game.movePieceDown();
+            view.render(game.getState());
+    }
+})
+
+view.render(game.getState());
