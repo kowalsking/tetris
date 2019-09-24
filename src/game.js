4115dbea04a5fee9a88805e184bb7e0498afdc93
Kozlenko <d.kozlenko@mediawayss.com>
2019-09-24 15:45:37 +0300

    refactoring figures

--- src/game.js
+++ src/game.js
@@ -25,15 +25,10 @@ export default class Game {
         [0,0,0,0,0,0,0,0,0,0]
     ];
 
-    activePiece = {
-        x: 0,
-        y: 0,
-        blocks: [
-            [0,1,0],
-            [1,1,1],
-            [0,0,0],
-        ]
-    };
+    activePiece = this.createPiece();
+
+    nextPiece = this.createPiece();
+
 
     getState() {
         const playfield = this.createPlayfield();
@@ -74,6 +69,73 @@ export default class Game {
         return playfield;
     }
 
+    createPiece() {
+        const index = Math.floor(Math.random() * 7);
+        const type = 'IJLOSTZ'[index];
+        const piece = {};
+
+        switch (type) {
+            case 'I':
+                piece.blocks = [
+                    [0,0,0,0],
+                    [1,1,1,1],
+                    [0,0,0,0],
+                    [0,0,0,0]
+                ];
+                break;
+            case 'J':
+                piece.blocks = [
+                    [0,0,0],
+                    [2,2,2],
+                    [0,0,2]
+                ];
+                break;
+            case 'L':
+                piece.blocks = [
+                    [0,0,0],
+                    [3,3,3],
+                    [3,0,0]
+                ];
+                break;
+            case 'O':
+                piece.blocks = [
+                    [0,0,0,0],
+                    [0,4,4,0],
+                    [0,4,4,0],
+                    [0,0,0,0]
+                ];
+                break;
+            case 'S':
+                piece.blocks = [
+                    [0,0,0],
+                    [0,5,5],
+                    [5,5,0]
+                ];
+                break;
+            case 'T':
+                piece.blocks = [
+                    [0,0,0],
+                    [6,6,6],
+                    [0,6,0]
+                ];
+                break;
+            case 'Z':
+                piece.blocks = [
+                    [0,0,0],
+                    [7,7,0],
+                    [0,7,7]
+                ];
+                break;
+
+            default:
+                throw new Error('Неизвестный тип фигуры!')
+        }
+
+        piece.x = Math.floor((10 - piece.blocks[0].length) / 2);
+        piece.y = -1;
+        return piece;
+    }
+
     movePieceLeft() {
         this.activePiece.x -= 1;
 
@@ -96,6 +158,8 @@ export default class Game {
         if (this.hasCollision()) {
             this.activePiece.y -= 1;
             this.lockPiece();
+            this.clearLines();
+            this.updatePieces();
         }
     }
 
@@ -160,5 +224,39 @@ export default class Game {
                 }
             }
         }
+    }
+
+    clearLines() {
+        const rows = 20;
+        const columns = 10;
+        let lines = [];
+
+        for (let y = rows - 1; y >= 0; y--) {
+            let numberOfBlocks = 0;
+
+            for (let x = 0; x < columns; x++) {
+                if (this.playfield[y][x]) {
+                    numberOfBlocks += 1;
+                }
+            }
+
+            if (numberOfBlocks === 0) {
+                break;
+            } else if (numberOfBlocks < columns) {
+                continue;
+            } else if (numberOfBlocks === columns) {
+                lines.unshift(y);
+            }
+        }
+
+        for (let index of lines) {
+            this.playfield.splice(index, 1);
+            this.playfield.unshift(new Array(columns).fill(0));
+        }
+    }
+
+    updatePieces() {
+        this.activePiece = this.nextPiece;
+        this.nextPiece = this.createPiece();
     }
 }
