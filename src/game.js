a25bcb10080ae06662d12b96d926a0964664c33d
Kozlenko <d.kozlenko@mediawayss.com>
2019-09-24 15:56:16 +0300

    add levels

--- src/game.js
+++ src/game.js
@@ -1,7 +1,13 @@
 export default class Game {
+    static points = {
+        '1': 40,
+        '2': 100,
+        '3': 300,
+        '4': 1200
+    };
+
     score = 0;
     lines = 0;
-    level = 0;
     playfield = [
         [0,0,0,0,0,0,0,0,0,0],
         [0,0,0,0,0,0,0,0,0,0],
@@ -28,6 +34,10 @@ export default class Game {
     activePiece = this.createPiece();
 
     nextPiece = this.createPiece();
+
+    get level() {
+        return Math.floor(this.lines * 0.1);
+    }
 
 
     getState() {
@@ -158,7 +168,8 @@ export default class Game {
         if (this.hasCollision()) {
             this.activePiece.y -= 1;
             this.lockPiece();
-            this.clearLines();
+            const clearedLines = this.clearLines();
+            this.updateScore(clearedLines);
             this.updatePieces();
         }
     }
@@ -253,6 +264,17 @@ export default class Game {
             this.playfield.splice(index, 1);
             this.playfield.unshift(new Array(columns).fill(0));
         }
+
+        return lines.length;
+    }
+
+    updateScore(clearedLines) {
+        if(clearedLines > 0) {
+            this.score += Game.points[clearedLines] * (this.level + 1);
+            this.lines += clearedLines;
+            console.log(this.score, this.lines);
+        }
+        console.log(this.level);
     }
 
     updatePieces() {
